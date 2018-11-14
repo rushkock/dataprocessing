dcd912de9c447c50bb21b6c070768a162593a910
Ruchella Kock <ruchella_kock@hotmail.com>
2018-11-14 17:22:25 +0100

    e & r

---
+++ homework/week_2/eda/eda.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# Name: Ruchella Kock
+# Student number: 1815458
+"""
+This script visualizes data obtained from a .csv file
+Contains the following information:
+Country, Region, Pop. Density (per sq. mi.),
+Infant mortality (per 1000 births), GDP ($ per capita) dollars
+Creates a boxplot of infant mortality
+Creates a histogram of GDP
+"""
+import csv
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def is_outlier(mean, std, data, data_name):
+    """
+    This function finds outliers in the data
+    It removes them based on the assumption that values higher than 95% outliers are
+    """
+    # find the 95%
+    cut_off = std * 3
+    # find the highest logical value and lowest value
+    lower = mean - cut_off
+    upper = mean + cut_off
+    outliers = [x for x in data if x < lower or x > upper]
+    print(f"{data_name} outliers: {len(outliers)}")
+    outliers_removed = [x for x in data if x >= lower and x <= upper]
+    return outliers_removed
+
+def main():
+    # define the values that are to be considered as NaN
+    na_values = ["unknown", "unknow"]
+
+    # put the columns to load in datframe into a list
+    columns = ["Country", "Region","Pop. Density (per sq. mi.)",
+               "Infant mortality (per 1000 births)", "GDP ($ per capita) dollars"]
+    names_columns = ["Region", "Pop_density", "Infant_mortality", "GDP"]
+
+    # read the csv into a dataframe
+    df = pd.read_csv("input.csv", index_col= 0, usecols= columns, na_values = na_values, decimal=",")
+    df.columns = names_columns
+
+    # -------------preprocessing--------------
+    # remove all non digits to GDP and make it of type numeric
+    df['GDP'].replace(regex=True,inplace=True,to_replace=r'\D',value=r'')
+    df["GDP"] = pd.to_numeric(df["GDP"])
+
+    # note I was unable to transform the float to int, couldn't find a method in pandas
+    # that works if you have NAN values
+    # tried:
+    # df["GDP"] = df["GDP"].apply(lambda x: int(x) if x > 0.0 else x)
+    # df["GDP"] = pd.to_numeric(df["GDP"], downcast = "signed")
+    # astype
+
+    # remove extra spaces from region
+    df["Region"] = df["Region"].apply(lambda x: x.rstrip())
+
+    # check how many outliers the variables have
+    print("OUTLIERS")
+    is_outlier(df["Pop_density"].mean(), df["Pop_density"].std(), df["Pop_density"], "Pop density")
+    data = is_outlier(df["GDP"].mean(), df["GDP"].std(), df["GDP"], "GDP")
+    is_outlier(df["Infant_mortality"].mean(), df["Infant_mortality"].std(), df["Infant_mortality"], "Infant mortality")
+    print()
+
+    # ----------- GDP ----------------
+    # get the central tendency of the GDP
+    print("CENTRAL TENDENCY")
+    mean_GDP = df["GDP"].mean()
+    median_GDP = df["GDP"].median()
+    mode_GDP = df["GDP"].mode()[0]
+    std_GDP = df["GDP"].std()
+    print(f"Mean GDP = {mean_GDP}\nMedian GDP = {median_GDP}\nMode GDP = {mode_GDP}\nStandard deviation GDP = {std_GDP}")
+    print()
+
+    # check for outliers and remove them, plot the GDP in a histogram
+    plt.subplot(1, 2, 1)
+    hist = plt.hist(data, bins=50)
+    plt.title("GDP")
+    plt.xlabel("$ per capita")
+    plt.ylabel("Number of countries")
+    """
+    In this histogram we see most countries have a GDP of 10000 or lower
+    We see the general trend that not many country have a high GDP
+    """
+
+    # ----------- Infant mortality ----------------
+    # 5 number summary of infant mortality
+    print("5 NUMBER SUMMARY")
+    print(df.Infant_mortality.describe())
+    print()
+
+    # make the boxplot
+    plt.subplot(1, 2, 2)
+    df["Infant_mortality"].plot(kind="box", title = "Infant mortality (per 1000 births)", label="")
+    plt.suptitle("")
+    """
+    In this boxplot we see a mean of around 20
+    We see 5 outliers that may influence the data 
+    The 100% is around 124 whereas the max is 191 This is quite a "big" difference
+    """
+
+    plt.show()
+
+    # rename columns to old names
+    names = ["Region", "Pop. Density (per sq. mi.)","Infant mortality (per 1000 births)", "GDP ($ per capita) dollars"]
+    df.columns = names
+    # write to json file
+    df.to_json(path_or_buf="eda.json", orient='index')
+
+if __name__ == '__main__':
+    main()
---
+++ homework/week_2/regression/regression.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+# Name: Ruchella Kock
+# Student number: 1815458
+"""
+Perform a simple linear regression, check assumptions and write analysis
+"""
+import pandas as pd
+from scipy import stats
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import statsmodels.stats.api as sms
+from statsmodels.stats import outliers_influence
+from statsmodels.graphics.regressionplots import abline_plot
+import matplotlib.pyplot as plt
+import scipy as sp
+
+def test_assumptions(res, df):
+    """
+    checks the assumptions of:
+    Linearity :
+        - Chosen method is linear_harvey_collier it assumes:
+        - "The Null hypothesis is that the regression is correctly modeled as linear."
+    Homoscedasticity :
+        - Chosen test is "Breusch-Pagan Lagrange Multiplier test for heteroscedasticity"
+        - The Null hypothesis is that there is independent error variance
+    Multicollinearity :
+        - Chosen test Variance Inflation Factor (VIF)
+    Outliers :
+        - Chosen tests:
+        Cooks distance indicates influential points that can influence the regression
+        Standardized residuals checks if there are outliers on the dependant variable
+        Boxplot checks if there outliers on the independent variable
+        Influence plot: One plot that gives a summary of the outliers/ influential points
+    Normality :
+        - Chosen method P-P plot
+    """
+    # get the residuals
+    pred_val = res.fittedvalues.copy()
+    residuals = res.resid
+
+    # counter to see how many assumptions are met
+    counter = 0
+
+    # check for linearity/ homoscedasticity visually
+    plt.subplot(1, 2, 1)
+    plt.scatter(pred_val, residuals)
+    plt.axhline()
+    plt.ylabel("Residuals")
+    plt.xlabel("Predicted values")
+    plt.title("Fig 1: Linearity/homoscedasticity check")
+    """
+    ----------- Fig 1 analysis -----------------
+    The data looks approximately linear. (Evenly divided across the line)
+    However as seen on the upper right side there are many values there
+    whereas under the line there arent as many na_values
+    This implies that the assumption of homoscedasticity may not be met
+    """
+
+    # check linearity assumption statistically
+    print("\nCHECK ASSUMPTIONS")
+    print("LINEARITY")
+    print("At a = 0.01, ", end="")
+    linearity_assump = sms.linear_harvey_collier(res)
+    if linearity_assump[1] < 0.01:
+        print(f"Null hypothesis is rejected (F value: {linearity_assump[0]}, P value: {linearity_assump[1]})")
+        print("This implies that there is no evidence of a linear relationship, assumption is NOT met")
+        counter = counter + 1
+    else:
+        print(f"Null hypothesis is not rejected (F value: {linearity_assump[0]}, P value: {linearity_assump[1]})")
+        print("This implies that there is evidence of a linear relationship, assumption is met")
+    print()
+
+
+    # check homoscedasticity assumption statistically
+    print("HOMOSCEDASTICITY")
+    test = sms.het_breuschpagan(res.resid, res.model.exog)
+    print("At a = 0.01, ", end="")
+    if test[1] < 0.01:
+        print(f"Null hypothesis is rejected (F value: {test[2]}, P value: {test[1]})")
+        print("This implies that there is dependent error variance, assumption is NOT met\n")
+        counter = counter + 1
+    else:
+        print(f"Null hypothesis is not rejected (F value: {test[2]}, P value: {test[1]})")
+        print("This implies that there is no dependent error variance, assumption is met\n")
+
+
+    # check multicollinearity assumption
+    print("MULTICOLLINEARITY")
+    VIF = outliers_influence.variance_inflation_factor(res.model.exog, 0)
+    print(f"VIF: {VIF}")
+    if VIF < 10:
+        print("There is no evidence of multicollinearity, VIF is smaller than 10")
+        print("Assumption is met\n")
+    else:
+        print("There is evidence of multicollinearity, VIF is bigger than 10")
+        print("Assumption is NOT met\n")
+        counter = counter + 1
+
+    # check for outliers
+    print("OUTLIERS")
+    oi = outliers_influence.OLSInfluence(res)
+    outliers = oi.summary_frame()
+
+    # check for influential cases
+    df_cook = outliers["cooks_d"]
+    df_residual = outliers["standard_resid"]
+    length_cook = len(df_cook[df_cook > 1])
+    if length_cook > 0:
+        print(f"There are {length_cook} influential points")
+        counter = counter + 1
+    else:
+        print("There are 0 influential points")
+
+    # check for outliers on depndant variable
+    max = len(df_residual[df_residual > 3])
+    min = len(df_residual[df_residual < -3])
+    length_residuals =  max + min
+    if length_residuals > 3 :
+        print(f"There are {length_residuals} outliers on dependant variable\n")
+        counter = counter + 1
+    else:
+        print("There are 0 outliers on dependant variable\n")
+
+    # check for outlier on independant variable
+    plt.subplot(1, 2, 2)
+    plt.boxplot(df["Deathrate"])
+    plt.title("Fig 2: Deathrate (independant variable)")
+    """
+    ------------ Fig 2 analysis -----------
+    The boxplot shows a mean of approx 7.
+    However there are outliers as high as approx 29self.
+    Moreover, it shows many outliers
+    Based on the boxplox, it may be concluded that
+    there are some outliers on the independant variable
+    """
+
+    # influence plot
+    sm.graphics.influence_plot(res, criterion="cooks")
+    plt.title("Fig 3: Influence Plot")
+
+    """
+    ---------Fig 3 analysis ----------
+    residuals are in one word "errors", studentizing makes it able to compare the residuals
+    * leverage indicates how far away the independent variable values of an
+      observation are from those of the other observations.
+    A few countries such as (Iraq, Namibia etc) have high std residuals,
+    however these countries have low leverage values
+    (This is sort of surprising considering the boxplot of the independant variable)
+    Most influential (as the criterion is cooks which gives the most influential cases)
+    Are the countries (Lesotho, Swalandia, Botswana) have both high std res and leverage values
+    * source: https://en.wikipedia.org/wiki/Leverage_(statistics)
+    """
+
+
+    # check for normality
+    probplot = sm.ProbPlot(residuals, fit=True)
+    probplot.ppplot(line="r")
+    plt.title("Fig 4: P-P Plot test of Normality")
+    """
+    ---------------Fig 4 analysis ------------
+    The p-p plot looks approximately normal (The blue dots are close to the red line)
+    we may assume that the assumption is met
+    """
+
+    return counter
+
+def main():
+    """
+    Chosen variables are Literacy and Deathrate
+    """
+    # define the values that are to be considered as NaN
+    na_values = ["unknown", "unknow"]
+
+    # choose columns and rename them
+    columns = ["Country", "Literacy (%)", "Deathrate"]
+    names_columns = ["Literacy" , "Deathrate"]
+
+    # read the csv into a dataframe
+    df = pd.read_csv("input.csv", index_col= 0, usecols= columns, na_values = na_values, decimal=",")
+    df.columns = names_columns
+
+    # drop NAN values from columns
+    df = df.dropna()
+
+    # -------------OLS-------------------------
+    mod = smf.ols(formula="Literacy ~ Deathrate", data=df)
+    res =  mod.fit()
+    print(res.summary())
+
+    # check if assumptions are met
+    counter = test_assumptions(res, df)
+
+    # anlyzise regression data
+    print("ANALYSIS")
+    print(f"F value : {res.fvalue}")
+    print(f"P value : {res.f_pvalue}")
+    if res.f_pvalue < 0.01:
+        print(f"The null hypothesis of no relationship between {columns[1]} and {columns[2]} is rejected")
+        print("This means we may assume there is a linear relationship between them")
+    else:
+        print(f"The null hypothesis of no relationship between {columns[1]} and {columns[2]} is not rejected")
+        print("This means we may assume there is no relationship between them")
+
+    if counter > 0:
+        print(f"However be wary of the results because {counter} assumption(s) is(are) not met\n")
+
+    r_squared = res.rsquared
+    print(f"R squared/ Variance Explained For(VAF) : {r_squared}")
+    print(f"{r_squared * 100:.2f} % of the dependant variable {columns[1]} is explained by the independant variable {columns[2]}\n")
+
+    # ----------- scipy linear regression-----------
+    # note I use this for two reasons the intercept and slope are easy to find
+    # moreover so I can compare if the two methods are identical
+    regression = stats.linregress(y = df["Literacy"], x = df["Deathrate"])
+
+    # show regression formula
+    print("REGRESSION FORMULA")
+    intercept = regression[1]
+    slope = regression[0]
+    if slope < 0:
+        print(f"{round(intercept,2):.2f} {round(slope,2):.2f} * {columns[2]}")
+    else:
+        print(f"{round(intercept,2):.2f} + {round(slope,2):.2f} * {columns[2]}")
+
+    # Plot the regression line
+    ax = df.plot(x="Deathrate", y='Literacy', kind='scatter')
+    abline_plot(model_results= res, ax=ax)
+    plt.xlabel("Deathrate")
+    plt.ylabel("Literacy")
+    plt.title("Fig 5: Regression Plot")
+    """
+    -------- Fig 5 analysis -----------
+    With a high percentage of literacy, Deathrate is lower.
+    There is a weak negative relationship between deathrate and literacy.
+    This means the lower the literacy the higher the deathrate.
+    """
+    plt.show()
+
+if __name__ == '__main__':
+    main()
